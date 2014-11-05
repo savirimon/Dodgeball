727276c108867a103491d04f61744730d5525732
savirimon <sasi@gatech.edu>
2014-11-04 23:10:10 -0500

    Ball continues moving post-throw
    -Changed ball velocity in throw function so it continues moving after
    player stops manipulating
    -Unbind ball to player so does not move with player post-throw

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,20 +7,37 @@ public class Ball : MonoBehaviour {
 	public Vector3 velocity;
 	public bool isNeutral;
 	public Player p;
+	private float scaleFactor;
 
 	void OnTriggerEnter2D(Collider2D other){
 		p = other.GetComponent<Player>();
+		if(other.tag == "Player"){
+			if(isNeutral){
+				owner = p.team;
+				Debug.Log("owner: " + owner);
+				SetColor(owner);
+				p.b = this;
+				p.b.transform.parent  = p.transform;
+			}else{
+				p.health--;
+				Debug.Log("decrement health");
+			}
+		}
 
-		if(isNeutral){
-			owner = p.team;
-			Debug.Log("owner: " + owner);
-			SetColor(owner);
-			p.b = this;
-			p.b.transform.parent  = p.transform;
-		}else
-		{
-			p.health--;
-			Debug.Log("decrement health");
+		if(other.tag == "Top"){
+			velocity = new Vector3(velocity.x, -velocity.y);
+		}
+
+		if(other.tag == "Bottom"){
+
+		}
+
+		if(other.tag == "Left"){
+
+		}
+
+		if(other.tag == "Right"){
+
 		}
 
 	}
@@ -45,6 +62,7 @@ public class Ball : MonoBehaviour {
 	}
 
 	void MoveBall(){
+		this.transform.Translate (velocity * Time.deltaTime * scaleFactor);
 	}
 
 	void SetNeutral(){
@@ -54,9 +72,12 @@ public class Ball : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		isNeutral = true;
+		velocity = new Vector3(0,0);
+		scaleFactor = 4f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		MoveBall();
 	}
 }
