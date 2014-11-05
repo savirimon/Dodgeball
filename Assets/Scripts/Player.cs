ff352fe3df988ea52c5b917d5651e56c51b359ef
mbhuet <mbhuet@gmail.com>
2014-11-05 14:44:23 -0500

    Player Controls
    I used XInput for player controls, it's a better way of using Xbox
    controllers with unity than the Input manager. set the playerNum to
    whichever controller number should control the player, 0 will default to
    keyboard controls. I also set up collision with the boundaries and mid
    line.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 
 public enum Team{ONE, TWO};
 
 public class Player : MonoBehaviour {
+	public int playerNum;
+
 	public Team team;
+
 	public int health;
 	public float speed;
 	public Ball b;
 	public bool isThrowing;
 	public float scaleFactor;
 	public bool againstWall;
+
+	protected PlayerIndex gamepadNum;
+	protected GamePadState gamepad;
+
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "Wall"){
@@ -37,6 +45,8 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		gamepad = GamePad.GetState(gamepadNum);
+		/*
 		if(!isThrowing){
 			Move();
 			if(Input.GetKeyDown("space")){
@@ -52,6 +62,11 @@ public class Player : MonoBehaviour {
 				Debug.Log("not throwing");
 			}
 		}
+		*/
+	}
+
+	void FixedUpdate(){
+		Move ();
 	}
 
 	void SetColor(Team t){
@@ -75,7 +90,47 @@ public class Player : MonoBehaviour {
 	}
 
 	void Move(){
-		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		this.transform.Translate (move * Time.deltaTime * speed);
+		//Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		//this.transform.Translate (move * Time.deltaTime * speed);
+
+		float leftX, leftY;
+
+		if (playerNum == 0){
+			leftX = Input.GetAxis ("Horizontal");
+			leftY = Input.GetAxis ("Vertical");
+		}
+
+		else{
+			leftX = gamepad.ThumbSticks.Left.X;
+			leftY = gamepad.ThumbSticks.Left.Y;
+		}
+		Vector3 move = new Vector3 (leftX, leftY, 0);
+
+		this.rigidbody2D.MovePosition (this.transform.position + move * Time.fixedDeltaTime * speed);
+
 	}
+
+	public void Init () {
+		switch (playerNum){
+		case 1: gamepadNum = PlayerIndex.One;
+			Debug.Log("Player 1 Registered");
+			break;
+		case 2: gamepadNum = PlayerIndex.Two;
+			Debug.Log("Player 2 Registered");
+			break;
+		case 3: gamepadNum = PlayerIndex.Three;
+			Debug.Log("Player 3 Registered");
+			break;
+		case 4 : gamepadNum = PlayerIndex.Four;
+			Debug.Log("Player 4 Registered");
+			break;
+		default :
+			playerNum = 0;
+			break;
+		}
+		
+		name = ("Player " + playerNum);
+		
+	}
+	
 }
