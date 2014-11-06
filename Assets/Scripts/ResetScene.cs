f40500be0e67a9117301a2d7587714a97a5d083f
Sasi <sasi@gatech.edu>
2014-11-06 12:12:18 -0500

    Added reset script to camera

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetScene : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Application.LoadLevel(Application.loadedLevel);		
+		}
+	
+	}
+}
